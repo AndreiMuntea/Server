1a43d53e4e14b8b7a4ce7dd57f0bc324b48394bd
Muntea Andrei <munteaandrei17@gmail.com>
2017-03-31 01:14:15 +0300

    Working on server

--- Server/Utils/include/constants.h
+++ Server/Utils/include/constants.h
@@ -6,12 +6,13 @@
 #define PIPE_PREFIX              TEXT("\\\\.\\pipe\\")
 #define PIPE_DEFAULT_NAME        TEXT("hardcoded")
 #define PIPE_NAME_MAX_SIZE       255
+#define DEFAULT_TIME_OUT_VALUE   10000
 
 #define REQUEST_BUFFER_SIZE      512
 #define REPLY_BUFFER_SIZE        512
 
 #define DEFAULT_LOG_FILE_NAME    TEXT("log.txt")
 
-#define DEFAULT_TIME_OUT_VALUE   1
+
 
 #endif //UTILS_CONSTANTS_H
